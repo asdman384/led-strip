7f680d1ff8bf3151475dda26ad93d1f1bf4a7aff
asdman384 <13420499+asdman384@users.noreply.github.com>
2025-01-14 20:26:29 +0200

    Implement BLE functionality and update LED control logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,97 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+// strip
 #define NUM_LEDS 299
 #define DATA_PIN D7
-#define BOOT_BUTTON_PIN 9 // Boot is usually GPIO 9 on the ESP32C3 boards.
-
 CRGB leds[NUM_LEDS];
 CRGB state_led[1];
+CLEDController *strip;
+CLEDController *state;
+
+// button
+#define BOOT_BUTTON_PIN 9 // Boot is usually GPIO 9 on the ESP32C3 boards.
 volatile bool buttonPressed = false;
 
-CLEDController *stripController;
-CLEDController *stateController;
+// BLE
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "a6237aee-71d3-4bb2-be69-07d81b255115"
+
+static uint8_t currentState = 0;
 
 void IRAM_ATTR handleButtonPress()
 {
   buttonPressed = true;
 }
 
-void setup()
+class MyServerCallbacks : public BLEServerCallbacks
 {
-  // Serial.begin(9600);
+  void onConnect(BLEServer *pServer)
+  {
+    Serial.println("BLEServer onConnect");
+  };
 
-  stripController = &FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
-  stateController = &FastLED.addLeds<WS2812, D10, GRB>(state_led, 1);
+  void onDisconnect(BLEServer *pServer)
+  {
+    Serial.println("BLEServer onDisconnect");
+    delay(500); // give the bluetooth stack the chance to get things ready
+    BLEDevice::startAdvertising();
+    Serial.println("BLEDevice::startAdvertising(); done");
+  }
+};
 
-  pinMode(BOOT_BUTTON_PIN, INPUT_PULLUP); // Set boot button pin as input with pull-up resistor
-  attachInterrupt(digitalPinToInterrupt(BOOT_BUTTON_PIN), handleButtonPress, FALLING);
+class MyCharacteristicCallbacks : public BLECharacteristicCallbacks
+{
+  void onWrite(BLECharacteristic *pCharacteristic)
+  {
+
+    uint8_t *data = pCharacteristic->getData();
+    for (size_t i = 0; i < pCharacteristic->getLength(); i++)
+    {
+      uint8_t d = data[i];
+      Serial.print(d);
+    }
+    state_led[0] = CRGB(data[0], data[1], data[2]);
+    Serial.println();
+
+    // std::string value = pCharacteristic->getValue();
+    // Serial.print("Received Value: ");
+    // for (int i = 0; i < value.length(); i++)
+    // {
+    //   Serial.print(value[i]);
+    // }
+  }
+};
+
+void setupBLE()
+{
+  delay(5000);
+  Serial.println("init LED-ESP32");
+  BLEDevice::init("LED-ESP32"); // set the device name
+
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  BLECharacteristic *pCharacteristic =
+      pService->createCharacteristic(CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_WRITE_NR | BLECharacteristic::PROPERTY_NOTIFY);
+  pCharacteristic->setValue("WNR");
+  pCharacteristic->setCallbacks(new MyCharacteristicCallbacks());
+
+  pService->start();
+
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06); // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("init LED-ESP32 done");
 }
 
 void clearAll()
@@ -34,6 +100,23 @@ void clearAll()
   {
     leds[i] = CRGB::Black;
   }
+}
+
+void setup()
+{
+  Serial.begin(9600);
+
+  strip = &FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
+  state = &FastLED.addLeds<WS2812, D10, GRB>(state_led, 1);
+
+  clearAll();
+  state_led[0] = CRGB::Black;
+  FastLED.show();
+
+  pinMode(BOOT_BUTTON_PIN, INPUT_PULLUP); // Set boot button pin as input with pull-up resistor
+  attachInterrupt(digitalPinToInterrupt(BOOT_BUTTON_PIN), handleButtonPress, FALLING);
+
+  setupBLE();
 }
 
 void snake()
@@ -59,46 +142,46 @@ void snake()
       }
     }
 
-    stripController->showLeds(255);
+    strip->showLeds(255);
     delay(20);
   }
 }
 
 void loop()
 {
-  static uint8_t state = 0;
   if (buttonPressed)
   {
-    state++;
+    currentState++;
+    Serial.println("currentState: " + String(currentState));
     buttonPressed = false;
   }
 
-  switch (state)
+  switch (currentState)
   {
   case 0:
     clearAll();
-    stripController->showLeds(255);
+    strip->showLeds(255);
 
     state_led[0] = CRGB::Blue;
-    stateController->showLeds(20);
-    delay(200);
+    state->showLeds(20);
+    delay(1000);
     break;
 
   case 1:
     state_led[0] = CRGB::Red;
-    stateController->showLeds(20);
-    delay(200);
+    state->showLeds(20);
+    delay(1000);
     break;
 
   case 2:
-    state_led[0] = CRGB::Black;
-    stateController->showLeds(20);
-    delay(200);
+    state_led[0] = CRGB::Green;
+    state->showLeds(20);
+    delay(1000);
     break;
 
   case 3:
-    state_led[0] = CRGB::Black;
-    stateController->showLeds(20);
+    state_led[0] = CRGB::GreenYellow;
+    state->showLeds(10);
     snake();
     break;
 
@@ -107,12 +190,17 @@ void loop()
     state_led[0] = CRGB::DeepPink;
     leds[0] = CRGB::DeepPink;
     FastLED.show();
-    delay(500);
+    delay(1000);
 
     break;
 
+  case 5:
+    delay(50);
+    state->showLeds(255);
+    break;
+
   default:
-    state = 0;
+    currentState = 0;
     break;
   }
 }
